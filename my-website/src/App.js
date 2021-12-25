23d9343aa2671dba5d6275cea4df3c44f8561952
Himanshu Sah <himanshusah41@gmail.com>
2021-12-25 14:19:29 +0545

    Added header and changed background color

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,7 +1,7 @@
-
+import Header from "./Components/Header";
 function App() {
   return (
-   <h1>Hello World</h1>
+   < Header />
   );
 }
 
---
+++ my-website/src/Components/Header.js
@@ -0,0 +1,15 @@
+
+import "../index.css";
+
+function Header(){
+    return(
+        <div className="header">
+            <button id="Home">Home</button>
+            <button id="About">About</button>
+            <button id="Software">Software</button>
+            <button id="Blogs">Blogs</button>
+            <button id="Contact">Contact</button>
+        </div>
+    )
+}
+export default Header;
