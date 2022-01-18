8850bbc85559a94c84dfa0a26e27b28b8dbbacff
Himanshu Sah <himanshusah41@gmail.com>
2022-01-18 14:46:40 +0545

    Copyright

--- my-website/src/Components/Footer.js
+++ my-website/src/Components/Footer.js
@@ -3,13 +3,19 @@ import {Link} from "react-router-dom";
 function Footer({buttonName}){
     
     return(
+        
         <div className="footer">
             <div className="footer-content">
                 <div className="footer-content-left">
                     <Link to={buttonName}><button>{buttonName}</button></Link>
                 </div>
             </div>
+            <div className="copyright">
+                <p>Copyright &copy; Himanshu Sah 2022</p>
+            </div>
         </div>
+        
+      
     )
 
 }
